9f26611705a79692cbc2129ebb314511ecb2fe88
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-08-20 01:14:16 -0700

    fix: URL compatibility (#12)

--- packages/validators/src/url/utils.ts
+++ packages/validators/src/url/utils.ts
@@ -5,16 +5,19 @@ const DYNAMIC_ROUTE_SEGMENT_REGEX = /\[\[?([^\]]+)\]?\]/g
 
 export const resolveRelativeUrl = (url: string, baseOrigin?: URL): URL => {
   if (!baseOrigin) {
-    if (!URL.canParse(url)) {
+    try {
+      return new URL(url)
+    } catch (error) {
       throw new UrlValidationError(`Invalid URL: ${url}`)
     }
-    return new URL(url)
   }
 
-  if (!URL.canParse(url, baseOrigin.href)) {
+  let normalizedUrl
+  try {
+    normalizedUrl = new URL(url, baseOrigin)
+  } catch (error) {
     throw new UrlValidationError(`Invalid URL: ${url}`)
   }
-  const normalizedUrl = new URL(url, baseOrigin)
 
   if (new URL(baseOrigin).origin !== normalizedUrl.origin) {
     throw new UrlValidationError(
