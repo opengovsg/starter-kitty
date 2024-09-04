7b336b3b43def522c6ad874f37de3d0c5492ff90
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-09-04 13:55:43 +0800

    feat: use path.relative to check base path

--- packages/validators/src/path/schema.ts
+++ packages/validators/src/path/schema.ts
@@ -8,7 +8,10 @@ import { isSafePath } from '@/path/utils'
 const createValidationSchema = (options: ParsedPathValidatorOptions) =>
   z
     .string()
+    // resolve the path relative to the Node process's current working directory
+    // since that's what fs operations will be relative to
     .transform((untrustedPath) => path.resolve(untrustedPath))
+    // resolvedPath is now an absolute path
     .refine((resolvedPath) => isSafePath(resolvedPath, options.basePath), {
       message: 'The provided path is unsafe.',
     })
--- packages/validators/src/path/utils.ts
+++ packages/validators/src/path/utils.ts
@@ -1,28 +1,25 @@
 import path from 'node:path'
 
-export const isSafePath = (
-  untrustedPath: string,
-  basePath: string,
-): boolean => {
+export const isSafePath = (absPath: string, basePath: string): boolean => {
   // check for poison null bytes
-  if (untrustedPath.indexOf('\0') !== -1) {
+  if (absPath.indexOf('\0') !== -1) {
     return false
   }
   // check for backslashes
-  if (untrustedPath.indexOf('\\') !== -1) {
+  if (absPath.indexOf('\\') !== -1) {
     return false
   }
-  // resolve the path relative to the Node process's current working directory
-  // since that's what fs operations will be relative to
-  const normalizedPath = path.resolve(untrustedPath) // normalizedPath is now an absolute path
 
   // check for dot segments, even if they don't normalize to anything
-  if (normalizedPath.includes('..')) {
+  if (absPath.includes('..')) {
     return false
   }
 
   // check if the normalized path is within the provided 'safe' base path
-  if (normalizedPath.indexOf(basePath) !== 0) {
+  if (path.resolve(basePath, path.relative(basePath, absPath)) !== absPath) {
+    return false
+  }
+  if (absPath.indexOf(basePath) !== 0) {
     return false
   }
   return true
