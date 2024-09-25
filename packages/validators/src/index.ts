a4b48e39bfde92a6497edf1e1c343e639b43167d
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-09-25 16:32:00 +0800

    refactor: path exports

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -6,9 +6,5 @@
 
 export type * from '@/common/errors'
 export * from '@/email'
-export type { EmailValidatorOptions } from '@/email/options'
 export * from '@/path'
-export type { PathValidatorOptions } from '@/path/options'
 export * from '@/url'
-export type * from '@/url/errors'
-export type { UrlValidatorOptions } from '@/url/options'
