5101e7a17c15c497b7fb6046645c5b7943b1f1ca
Eugene Lim <eugene.gvt@open.gov.sg>
2025-07-25 18:12:40 +0800

    fix: remove starter-kitty-fs (#42)

--- packages/safe-fs/src/sanitizers.ts
+++
@@ -1,8 +0,0 @@
-import { PathLike } from 'fs'
-import path from 'path'
-
-const LEADING_DOT_SLASH_REGEX = /^(\.\.(\/|\\|$))+/
-
-export const sanitizePath = (dangerousPath: PathLike, rootPath: string): string => {
-  return path.join(rootPath, path.normalize(dangerousPath.toString()).replace(LEADING_DOT_SLASH_REGEX, ''))
-}
